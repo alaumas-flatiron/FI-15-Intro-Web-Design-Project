9ab0ca941e2ad50a8928ff93307ffff23f64e12c
anl2000 <anl4540@gmail.com>
2015-07-16 15:20:55 +0000

    in progress

--- js/question-script.js
+++ js/question-script.js
@@ -1,5 +1,66 @@
 $ (document).ready(function(){
+   
+   var $userScore == 0;
   
+  $("#answer1a:eq(0)").click(function(){
+		$userScore += 1;
+    console.log($userScore);
+	});
+  
+   $("#answer1b:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+  $("#answer1c:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+  $("#answer1d:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+  $("#answer2a:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+   $("#answer2b:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+  $("#answer2c:eq(0)").click(function(){
+		$userScore += 1;
+    console.log($userScore);
+	});
+  
+  $("#answer2d:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+   $("#answer3a:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+   $("#answer3b:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+  $("#answer3c:eq(0)").click(function(){
+		$userScore = 0;
+    console.log($userScore);
+	});
+  
+  $("#answer3d:eq(0)").click(function(){
+		$userScore += 1;
+    console.log($userScore);
+	});
   
   
   
